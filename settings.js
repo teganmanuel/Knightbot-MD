31dc4d3d91efde72a10ad46ef9d41aaaaf182672
teganmanuel <140430394+teganmanuel@users.noreply.github.com>
2025-04-25 18:04:22 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '91702395151X', //Set your number here without + symbol, just add country code & number without any space
+  botName: "tegan_secret_life",
+  botOwner: 'Tegan M', // Your name
+  ownerNumber: '27694930545', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
